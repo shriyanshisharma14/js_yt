d51c3bd180406861370798eac5ea8d2702ae4b53
shriyanshisharma14 <76079253+shriyanshisharma14@users.noreply.github.com>
2024-05-01 08:46:43 +0000

    datatype

---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //Treat all Js code as newer version of js (if it is written then can't go back to older version for that file)
+//alert(3+3) //this throws error as we are running nodejs and there is different way to run it in node not like we used to run it in console of browser
+
+//console.log(5+5) console.log("Shri") //";" is expected or we can write each command in next line to not use ";"
+
+console.log(4
++ 5) //code readability should be high
+
+let name="shriyanshi"
+let age=20
+let isLoggedIn=false
+let state=null;
+
+
+/* Primitive Datatypes */
+//number => 2^53 range
+//bigint => (for number that exceeds above range)
+//string 
+//boolean
+//null => standalone value
+//undefined
+//symbols => uniquely addressing something
+
+/* Non-Primitive data type */
+//object
+
+console.log(typeof age) //number
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
